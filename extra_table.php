39cf68131e137acb00ca3b8e64c0e7eec5e1bb1a
rrifat <rrifat.alam.93@gmail.com>
2018-05-25 00:53:57 +0600

    Button table

---
+++ extra_table.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Admin</title>
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" type="text/css" href="resource/vendor/bootstrap/css/bootstrap.min.css" media="screen"/>
+
+    <!-- Custom Fonts -->
+    <link href="resource/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <h3 class="text-center">Button Table</h3>
+                <div class="table-responsive">
+                    <br><br>
+                    <table class="table table-hover table-bordered">
+                        <tr class="text-center">
+                            <td><button class="btn btn-default">Default</button></td>
+                            <td><button class="btn btn-primary">Primary</button></td>
+                            <td><button class="btn btn-success">Success</button></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
